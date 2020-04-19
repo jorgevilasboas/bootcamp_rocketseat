880d25c738f53f80346f824c49121d11f6019d88
jorgevilasboas <jorge.sergipe@gmail.com>
2020-04-19 17:29:50 -0300

    conceitos-react

---
+++ conceitos-dev/frontend/README.md
@@ -0,0 +1,15 @@
+# Conceitos ReactJS
+
+> Conceitos ReactJS do curso do Bootcamp da Rocketseat
+
+## Exemplo de uso
+
+Executar:
+
+`yarn webpack-dev-server --mode development`
+
+## Meta
+
+Jorge Vilas Boas – [@jorgevilasboas](https://twitter.com/dbader_org)
+
+[https://github.com/jorgevilasboas/github](https://github.com/dbader/)
---
+++ conceitos-dev/frontend/public/script.js
@@ -0,0 +1,12 @@
+/**
+ * Babel: Converter (transpilar) codigo do REact par aum codigo que o browser entenda
+ * WebPack: Para cada tipo de arquivo (.js, .css, .png), converter o codigo de uma maneira diferente *
+ * Loaders: babel-loader, css-loader, image-loader
+ *
+ * *** configurar babel ***
+ * yarn add @babel/cli @babel/core @babel/preset-react webpack webpack-cli
+ *
+ * *** configurar webpack ***
+ *
+ *
+ */
